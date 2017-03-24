281ccdf9324fcfdc84c10bc6c78e05406093fc88
Gogs <gogs@fake.local>
2017-03-25 00:20:56 +0100

    showing notes

--- pages/notes.js
+++ pages/notes.js
@@ -3,6 +3,17 @@ $(function(){
     if(token !== null)
     {
         var notes = sdk.run('PIZARRA','',token);
-        alert(notes[0].id);
+        var tpl = $("#news-template").html();
+
+        for(var item in notes.notes)
+        {
+            var html = tpl;
+            for(var prop in notes.notes[item])
+            {
+                html = html.replace('{{ ' + prop + ' }}', notes.notes[item][prop]);
+            }
+
+            $("#list-news").append(html);
+        }
     }
 });
