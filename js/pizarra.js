9fc3513f796e04396fa032a0dc70bc72c9b9a319
kumahacker <kumahavana@gmail.com>
2017-04-04 08:25:02 -0500

    xeros is not https

--- js/pizarra.js
+++ js/pizarra.js
@@ -4,7 +4,7 @@
  * @author @kumahacker
  */
 
-var sdk = new apretaste("https://xeros.co/");
+var sdk = new apretaste("http://xeros.co/");
 //var sdk = new apretaste("http://corex.wifi/");
 
 var pizarra = {
